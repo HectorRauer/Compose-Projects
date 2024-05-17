ac914f28faebd582a6f37293cb9725fdef608ab6
Hector Rauer <hector.rauer@gmail.com>
2024-05-17 17:29:16 -0300

    added icons

--- app/src/main/java/com/example/journey/MainActivity.kt
+++ app/src/main/java/com/example/journey/MainActivity.kt
@@ -6,11 +6,14 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.core.presentation.designsystem.AnalyticsIcon
 import com.example.core.presentation.designsystem.JourneyTheme
 import com.example.journey.ui.theme.journeyTheme
 
@@ -20,29 +23,10 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             JourneyTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    Icon(imageVector = AnalyticsIcon, contentDescription = null)
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    journeyTheme {
-        Greeting("Android")
     }
 }
