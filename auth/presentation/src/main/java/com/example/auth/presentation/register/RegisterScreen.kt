ec30d8e1fcf879e643158f460cbb4355b538a3b1
Hector Rauer <hector.rauer@gmail.com>
2024-05-31 16:18:36 -0300

    finished register screen ui implementation

--- auth/presentation/src/main/java/com/example/auth/presentation/register/RegisterScreen.kt
+++ auth/presentation/src/main/java/com/example/auth/presentation/register/RegisterScreen.kt
@@ -4,17 +4,21 @@ package com.example.auth.presentation.register
 
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.ClickableText
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.SpanStyle
@@ -24,13 +28,20 @@ import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.auth.domain.UserDataValidator
 import com.example.auth.presentation.R
 import com.example.core.presentation.designsystem.CheckIcon
+import com.example.core.presentation.designsystem.CrossIcon
 import com.example.core.presentation.designsystem.EmailIcon
+import com.example.core.presentation.designsystem.JourneyDarkRed
 import com.example.core.presentation.designsystem.JourneyGray
+import com.example.core.presentation.designsystem.JourneyGreen
 import com.example.core.presentation.designsystem.JourneyTheme
 import com.example.core.presentation.designsystem.Poppins
 import com.example.core.presentation.designsystem.components.GradientBackground
+import com.example.core.presentation.designsystem.components.JourneyActionButton
+import com.example.core.presentation.designsystem.components.JourneyPasswordTextField
 import com.example.core.presentation.designsystem.components.JourneyTextField
 import org.koin.androidx.compose.koinViewModel
 
@@ -119,9 +130,73 @@ private fun RegisterScreen(
                 additionalInfo = stringResource(id = R.string.must_be_valid_email),
                 keyboardType = KeyboardType.Email
             )
+            Spacer(modifier = Modifier.height(16.dp))
+            JourneyPasswordTextField(
+                state = state.password,
+                hint = stringResource(id = R.string.password),
+                title = stringResource(id = R.string.password),
+                isPasswordVisible = state.isPasswordVisible,
+                onTogglePasswordVisibility = {
+                    onAction(RegisterAction.OnTogglePasswordVisibilityClick)
+                },
+                modifier = Modifier.fillMaxWidth()
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            PasswordRequirement(
+                text = stringResource(
+                    id = R.string.at_least_x_characters, UserDataValidator.MIN_PASSWORD_LENGTH),
+                isValid = state.passwordValidationState.hasMinLength
+            )
+            Spacer(modifier = Modifier.height(4.dp))
+            PasswordRequirement(
+                text = stringResource(id = R.string.at_least_one_number),
+                isValid =state.passwordValidationState.hasNumber
+            )
+            Spacer(modifier = Modifier.height(4.dp))
+            PasswordRequirement(text = stringResource(id = R.string.contains_lowercase_character),
+                isValid = state.passwordValidationState.hasLowerCaseCharacter)
+            Spacer(modifier = Modifier.height(4.dp))
+            PasswordRequirement(text = stringResource(id = R.string.contains_uppercase_character),
+                isValid =  state.passwordValidationState.hasUpperCaseCharacter
+            )
+            Spacer(modifier = Modifier.height(32.dp))
+            JourneyActionButton(
+                text = stringResource(id = R.string.register),
+                isLoading = state.isResgistering,
+                enabled = state.canRegister,
+                modifier = Modifier.fillMaxWidth(),
+                onCLick = {
+                    onAction(RegisterAction.OnRegisterClick)
+                }
+            )
         }
     }
 
+}
+
+@Composable
+fun PasswordRequirement(
+    modifier: Modifier = Modifier,
+    text: String,
+    isValid: Boolean,
+
+    ) {
+    Row(
+        modifier = modifier,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Icon(
+            imageVector = if (isValid) CheckIcon else CrossIcon,
+            contentDescription = null,
+            tint = if(isValid) JourneyGreen else JourneyDarkRed
+        )
+        Spacer(modifier = Modifier.width(16.dp))
+        Text(
+            text = text,
+            color = MaterialTheme.colorScheme.onSurfaceVariant,
+            fontSize = 14.sp
+        )
+    }
 }
 
 @Preview
