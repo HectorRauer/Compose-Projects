efae00fb449269780c28b64d9869a87d47cc4946
Hector Rauer <hector.rauer@gmail.com>
2024-06-05 15:37:47 -0300

    added shared prefs config

--- app/src/main/java/com/example/journey/di/AppModule.kt
+++ app/src/main/java/com/example/journey/di/AppModule.kt
@@ -1,8 +1,21 @@
 package com.example.journey.di
 
+import android.content.SharedPreferences
+import androidx.security.crypto.EncryptedSharedPreferences
+import androidx.security.crypto.MasterKey
 import com.example.auth.data.EmailPatternValidator
 import com.example.auth.domain.PatternValidator
+import org.koin.android.ext.koin.androidApplication
 import org.koin.dsl.module
 
 val appModule = module {
+    single<SharedPreferences> {
+        EncryptedSharedPreferences(
+            androidApplication(),
+            "auth_pref",
+            MasterKey(androidApplication()),
+            EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+            EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
+        )
+    }
 }
