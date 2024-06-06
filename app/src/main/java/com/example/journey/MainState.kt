9e78b22b2aae8efeddd4906627987e0ec96fd442
Hector Rauer <hector.rauer@gmail.com>
2024-06-06 19:02:47 -0300

    added splash screen verification

---
+++ app/src/main/java/com/example/journey/MainState.kt
@@ -0,0 +1,6 @@
+package com.example.journey
+
+data class MainState (
+    val isLoggedIn: Boolean = false,
+    val isCheckingAuth: Boolean = false
+)
--- app/src/main/java/com/example/journey/di/AppModule.kt
+++ app/src/main/java/com/example/journey/di/AppModule.kt
@@ -5,7 +5,9 @@ import androidx.security.crypto.EncryptedSharedPreferences
 import androidx.security.crypto.MasterKey
 import com.example.auth.data.EmailPatternValidator
 import com.example.auth.domain.PatternValidator
+import com.example.journey.MainViewModel
 import org.koin.android.ext.koin.androidApplication
+import org.koin.androidx.viewmodel.dsl.viewModelOf
 import org.koin.dsl.module
 
 val appModule = module {
@@ -18,4 +20,5 @@ val appModule = module {
             EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
         )
     }
+    viewModelOf(::MainViewModel)
 }
